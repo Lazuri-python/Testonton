d102173a46b49840cc27f88c539733c39adb16c7
Totor le plus fort <totor@youpi.com>
2022-07-20 09:45:10 +0100

    Yooooooooo

---
+++ network-graph.py
@@ -0,0 +1,130 @@
+DIAS_STYLE='shape = plaintext, image = \"$IMG_DIR/dias.png\", size=500, fontsize=190, fontcolor=black'
+TEAM_STYLE='shape = plaintext, image = \"$IMG_DIR/DIAS_penguins.png\", size=500, fontsize=190, fontcolor=black'
+INTERNET_STYLE='shape = plaintext, image = \"$IMG_DIR/internet.svg-512.png\", size=150, fontsize=1, fontcolor=none'
+ICHEC_STYLE='shape = plaintext, image = \"$IMG_DIR/ichec.png\", size=75, fontsize=1, fontcolor=none'
+HEANET_STYLE='shape = plaintext, image = \"$IMG_DIR/HEAnet.512.png\", size=75, fontsize=1, fontcolor=none'
+ROUTER_STYLE='shape = circle, color = none, image = \"$IMG_DIR/dev-router.svg-512.png\", size=50'
+SITENAME_STYLE='fontsize=150, fontcolor=black'
+FW_STYLE='shape = plaintext, image = \"$IMG_DIR/dev-firewall.svg-512.png\ '
+ROUTER_BURL_STYLE= 'shape = circle, color = none, image = \"$IMG_DIR/dev-router-burlington.svg-512.png\", size=100'
+ROUTER_MERRION_STYLE='shape = circle, color = none, image = \"$IMG_DIR/dev-router-merrion.svg-512.png\", size=100'
+ROUTER_FITZ_STYLE='shape = circle, color = none, image = \"$IMG_DIR/dev-router-fitzwilliam.svg-512.png\", size=100'
+SWITCH_STYLE='shape = circle, color = none, image = \"$IMG_DIR/Switch.png-512.png\", size=50'
+DUMBSWITCH_STYLE='shape = circle, color = none, image = \"$IMG_DIR/Switch.png-512.png\", size=15'
+VLAN_STYLE='shape = square, color = none, image = \"$IMG_DIR/net-vlan.png\ '
+ANTENNA_STYLE='shape = plaintext, image = \"$IMG_DIR/dev-wireless.svg-256.png\ '
+WIFI_STYLE='shape = circle, color = none, image = \"$IMG_DIR/dev-wireless.svg-256.png\ '
+KVM_STYLE='fontsize = 12, image = \"$IMG_DIR/dev-server.svg-256.png\", shape = plaintext, fontcolor = black'
+VM_STYLE='fontsize = 7,  shape = plaintext, fontcolor = gray'
+#VM_STYLE="fontsize = 7,  shape = plaintext, fontcolor = lightgray"
+DEBIAN_STYLE='fontsize = 12, image = \"$IMG_DIR/os-debian.svg-256.png\", shape = plaintext, fontcolor = black'
+CENTOS_STYLE='fontsize = 12, image = \"$IMG_DIR/os-centos.svg-256.png\", shape = plaintext, fontcolor = black'
+FREEBSD_STYLE='fontsize = 12, image = \"$IMG_DIR/os-freebsd.png-256.png\", shape = plaintext, fontcolor = black'
+FREENAS_STYLE='fontsize = 12, image = \"$IMG_DIR/os-freenas.png\", shape = plaintext, fontcolor = black'
+WINDOWS_STYLE='fontsize = 12, image = \"$IMG_DIR/os-ms.png\", shape = plaintext, fontcolor = black'
+APPLE_STYLE='fontsize = 12, image = \"$IMG_DIR/os-apple-black.png\", shape = plaintext, fontcolor = black'
+SUN_STYLE='fontsize = 12, image = \"$IMG_DIR/os-sun.png\", shape = plaintext, fontcolor = black'
+DESKTOP_STYLE='fontsize = 12, image = \"$IMG_DIR/dev-computer.svg-64.png\", shape = plaintext, fontcolor = black'
+SERVER_STYLE='fontsize = 12, image = \"$IMG_DIR/dev-server3.png\", shape = plaintext, fontcolor = black'
+LAPTOP_STYLE='fontsize = 12, image = \"$IMG_DIR/dev-laptop.png\", shape = plaintext, fontcolor = black'
+PHONE_STYLE='fontsize = 6, image = \"$IMG_DIR/dev-phone.png\", shape = plaintext, fontcolor = lightblue'
+VCU_STYLE='fontsize = 6, image = \"$IMG_DIR/dev-vcu.png\", shape = plaintext, fontcolor = lightblue'
+VPN_STYLE='fontsize = 6, image = \"$IMG_DIR/vpn.png\", shape = plaintext, fontcolor = lightgray'
+PRINTER_STYLE='fontsize = 6, image = \"$IMG_DIR/printer.png\", shape = plaintext, fontcolor = lightblue'
+PRINTER3D_STYLE='fontsize = 6, image = \"$IMG_DIR/3dprinter.png\", shape = plaintext, fontcolor = lightblue'
+SEISM_STYLE='fontsize = 6, image = \"$IMG_DIR/seismo-black.png\", shape = plaintext, fontcolor = black, size=20'
+SENSOR_STYLE='fontsize = 6, image = \"$IMG_DIR/sensor.png\", shape = plaintext, fontcolor = lightblue'
+UPS_STYLE='fontsize = 6, image = \"$IMG_DIR/dev-ups.png\", shape = plaintext, fontcolor = black'
+SAN_STYLE='fontsize = 6, image = \"$IMG_DIR/dev-san.png\", shape = plaintext, fontcolor = black'
+MGMT_STYLE='fontsize = 6, image = \"$IMG_DIR/dev-mgmt.png\", shape = plaintext, fontcolor = black'
+KVMCAB_STYLE='fontsize = 6, image = \"$IMG_DIR/dev-station.svg-256.png\", shape = plaintext, fontcolor = black'
+SSH_STYLE='fontsize = 6, image = \"$IMG_DIR/ssh.png\", shape = plaintext, fontcolor = black'
+WHATISIT_STYLE='fontsize = 6, image = \"$IMG_DIR/dev-whatisit.png\", shape = plaintext, fontcolor = black'
+NONE_STYLE='fontsize = 1, image = \"$IMG_DIR/1px.png\", size = 1, color = none, fontcolor = black'
+DEFAULT_STYLE='fontsize = 8,  shape = circle, color = none'
+YOUAREHERE_STYLE='fontsize = 6, image = \"$IMG_DIR/you-are-here-arrow.png\", shape = plaintext, fontcolor = black, size=0'
+
+LINK_NA="color = red"
+LINK_KVM="width=1.5, color = lightgray"
+LINK_WIFI="color = green, dashes=\"[2,2,10,10]\""
+LINK_VPN="color = blue, dashes=true"
+LINK_TEL="color = lightblue"
+LINK_3G="color = orange, dashes=\"[2,2,10,10]\""
+LINK_10M="color = lightgray, dashes=true"
+LINK_100M="color = lightgray, dashes=true"
+LINK_1G="color = gray"
+LINK_1G_CORE="color = gray, width=2"
+LINK_10G="width=2, color = orange"
+LINK_VLAN="width=1.5, color = black"
+
+
+
+
+vlan=[ 1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 ,20 ,21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29 ,30 ,31 ]
+router=[ "router1" ]
+switch=[ "swa1", "swb", "swc","swa2","swa3","swa4","swa5","swa6","swa7" ]
+kvm=[ "kvm1", "kvm2" ]
+vm=[ "vm1", "vm2" ]
+desktop=[ "pc1", "pc2" ]
+
+
+connections = [
+ ( "router1", "swa1" ),
+ ( "router1", "swa2" ),
+ ( "router1", "swa3" ),
+ ( "router1", "swa4" ),
+ ( "router1", "swa5" ),
+ ( "router1", "swa6" ),
+ ( "router1", "swa7" ),
+ ( "swa1", "swb" ),
+ ( "swb", "kvm2" ),
+]
+
+xmax= 0
+ymax= 0
+
+
+def get_item_design(obj):
+    if obj in router:
+        return ROUTER_STYLE
+    elif obj in switch:
+        return SWITCH_STYLE
+    elif obj in kvm:
+        return KVM_STYLE
+    elif obj in vm:
+        return VM_STYLE
+    elif obj in desktop:
+        return DESKTOP_STYLE
+    else:
+        return WHATISIT_STYLE
+
+def get_connections(obj):
+    l = []
+    for e in connections:
+        if e[0] == obj:
+            l.append(e[1])
+    return l
+
+def get_position(obj,x=xmax,y=ymax):
+    L = get_connections(obj)
+    print(obj,x,y)
+    pos[obj] = (x,y,get_item_design(obj))
+    if L != []:
+        dif = 0
+        for e in L:
+            dif += 250
+            print(e,(x-x/2)+dif,(y-y/2))
+            get_position(e,(x-x/2)+dif,(y-y/2))
+
+def write(dict):
+    with open("burl2.js", 'w') as file:
+        file.write("var DOTstring='';\nDOTstring+='graph G {';\n")
+        for key in dict:
+            file.write("DOTstring += '"+ str(key) + " [ "+dict[key][2]+",   x = "+str(dict[key][0])+',  y = '+str(dict[key][0])+"   ]';\n")
+        file.write("DOTstring+='}';")
+        file.close()
+
+
+pos = {"router1": (xmax,ymax,get_item_design("router1"))}
+get_position("router1")
+write(pos)
